74b3307e97150ad457fe3513fc018ea162a972b0
Shahd Mustafa <99287558+shahdmu34@users.noreply.github.com>
2023-04-13 14:06:23 -0400

    -commit

---
+++ app/src/main/java/com/example/project4_cis436/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.project4_cis436
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.project4_cis436.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        val buttonSearch = binding.SearchButton
+
+        buttonSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_bookSearchFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/example/project4_cis436/MainActivity.kt
+++ app/src/main/java/com/example/project4_cis436/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.project4_cis436
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
 
